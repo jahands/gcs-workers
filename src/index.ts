de8f9628d91fdeea4daf51c20079a16df29b60f0
Jacob Hands <jacob@gogit.io>
2023-07-11 22:57:20 -0500

    Add kv

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,57 @@
-import GoogleAuth, { GoogleKey } from './google'
+import GoogleAuth, { GoogleKey } from "./google";
 
 export interface Env {
-	GCS_JSON_KEY: string
+  GCS_JSON_KEY: string;
+  KV: KVNamespace;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		const bucketName = 'play-powershell'
-		const fileName = 'DB/Play/JhaContentDB.csv'
-		// https://developers.google.com/identity/protocols/oauth2/scopes
-		const scopes: string[] = ['https://www.googleapis.com/auth/devstorage.full_control']
-		const googleAuth: GoogleKey = JSON.parse(env.GCS_JSON_KEY)
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    const bucketName = "play-powershell";
+    const fileName = "DB/Play/JhaContentDB.csv";
+    // https://developers.google.com/identity/protocols/oauth2/scopes
+    const scopes: string[] = [
+      "https://www.googleapis.com/auth/devstorage.full_control",
+    ];
+    const googleAuth: GoogleKey = JSON.parse(env.GCS_JSON_KEY);
 
-		// Initialize the service
-		const oauth = new GoogleAuth(googleAuth, scopes)
-		const token = await oauth.getGoogleAuthToken()
+    // Initialize the service
+    const oauth = new GoogleAuth(googleAuth, scopes);
+    const start = Date.now();
+    let token = await env.KV.get("token");
+    const end = Date.now();
+    console.log(`KV get took ${end - start}ms`);
+    if (!token) {
+      console.log("getting new token");
+      token = await oauth.getGoogleAuthToken();
 
-        // Example with Google Cloud Storage
-		const res = await fetch(`https://storage.googleapis.com/storage/v1/b/${bucketName}/o/${encodeURIComponent(fileName)}`, {
-			method: 'PATCH',
-			body: JSON.stringify({customTime: new Date().toISOString()}),
-			headers: {
-				'Authorization': `Bearer ${token}`,
-				'Content-Type': 'application/json'
-			},
-		})
+      if (!token) {
+        return new Response("Failed to get token");
+      }
 
-		return new Response(res.body);
-	},
+      await env.KV.put("token", token, { expirationTtl: 60 });
+    }
+
+    // Example with Google Cloud Storage
+    const res = await fetch(
+      `https://storage.googleapis.com/storage/v1/b/${bucketName}/o/${encodeURIComponent(
+        fileName
+      )}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({ customTime: new Date().toISOString() }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return new Response(res.body);
+  },
 };
-
-
